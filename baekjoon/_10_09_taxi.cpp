a2ab1483fcea27c82fb52cc6c291c373dc90e8e7
sy_Kim <qweqwz@naver.com>
2020-02-18 10:20:16 +0900

    taxi bulabula

---
+++ baekjoon/_10_09_taxi.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+using namespace std;
+int main(){
+    int R;
+    cin>>R;
+    cout<<fixed;
+    cout.precision(6);
+    cout<<double(R*R*M_PI)<<"\n";
+    cout<<double(R*R*2);
+    return 0;
+
+}
